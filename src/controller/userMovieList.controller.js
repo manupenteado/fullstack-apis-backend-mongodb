6d2c232c54430c015e77e19c0f79afa6cd3fb1da
Rodrigo Peixoto <107653217+ROpeixoto@users.noreply.github.com>
2025-06-03 00:14:05 +0000

    mudanças no controller (corrigindo userid)

--- src/controller/userMovieList.controller.js
+++ src/controller/userMovieList.controller.js
@@ -4,7 +4,7 @@ export const addMovie = async (req, res) => {
   const { movieId, listType } = req.body;
   try {
     const entry = await UserMovieList.create({
-      user: req.user.id,
+      user: req.userId, // <-- Corrigido aqui
       movieId,
       listType
     });
@@ -17,7 +17,7 @@ export const addMovie = async (req, res) => {
 export const getMovies = async (req, res) => {
   try {
     const movies = await UserMovieList.find({
-      user: req.user.id,
+      user: req.userId, // <-- Corrigido aqui
       listType: req.params.listType
     });
     res.json(movies);
