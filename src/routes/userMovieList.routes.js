826b33c43d6ac8563428d9ed0cbf4a6a2f525fa1
Emanuele Penteado <163652019+manupenteado@users.noreply.github.com>
2025-06-26 12:56:20 +0000

    consertando importação

--- src/routes/userMovieList.routes.js
+++ src/routes/userMovieList.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import verifyToken from '../middleware/jwt.token.middleware.js';
-import { addMovie, getMovies, deleteMovieFromList } from '../controller/userMovieList.controller.js';
+import { addMovie, getMovies, deleteMovieFromList, rateMovie } from '../controller/userMovieList.controller.js';
 
 const router = express.Router();
 
